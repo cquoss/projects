7427a2ae956df397ad8748fa176f76d948a0183d
cquoss <clemens@quoss.de>
2025-09-26 18:49:37 +0200

    populate-lib.bash: fix javax.jms-api group id

--- mq-jdk8-client/bin/populate-lib.bash
+++ mq-jdk8-client/bin/populate-lib.bash
@@ -7,4 +7,4 @@ echo 'Copy commons-cli version 1.10.0 ...'
 cp -v "${2}/commons-cli/commons-cli/1.10.0/commons-cli-1.10.0.jar" 'lib/'
 
 echo 'Copy javax.jms-api version 2.0.1 ...'
-cp -v "${2}/javax.jms/javax.jms-api/2.0.1/javax.jms-api-2.0.1.jar" 'lib/'
+cp -v "${2}/javax/jms/javax.jms-api/2.0.1/javax.jms-api-2.0.1.jar" 'lib/'
