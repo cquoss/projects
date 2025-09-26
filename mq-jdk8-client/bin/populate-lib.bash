d9889fe3d7d93c3385ee1f143c302d1625a78818
cquoss <clemens@quoss.de>
2025-09-26 18:47:39 +0200

    populate-lib.bash: add javax.jms-api version 2.0.1
    populate.bash: add apost and quotes

--- mq-jdk8-client/bin/populate-lib.bash
+++ mq-jdk8-client/bin/populate-lib.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo 'Copy app jar ...'
-cp -v ${1}/target/mq-jdk8-client-1.0.jar lib/
+cp -v "${1}/target/mq-jdk8-client-1.0.jar" 'lib/'
 
 echo 'Copy commons-cli version 1.10.0 ...'
-cp -v ${2}/commons-cli/commons-cli/1.10.0/commons-cli-1.10.0.jar lib/
+cp -v "${2}/commons-cli/commons-cli/1.10.0/commons-cli-1.10.0.jar" 'lib/'
+
+echo 'Copy javax.jms-api version 2.0.1 ...'
+cp -v "${2}/javax.jms/javax.jms-api/2.0.1/javax.jms-api-2.0.1.jar" 'lib/'
