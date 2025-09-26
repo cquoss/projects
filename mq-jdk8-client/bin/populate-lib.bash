34d0e4b151c9e93b9203f91cffdc19f121829220
cquoss <clemens@quoss.de>
2025-09-26 18:56:10 +0200

    populate-lib.bash: add logback version 1.5.18

--- mq-jdk8-client/bin/populate-lib.bash
+++ mq-jdk8-client/bin/populate-lib.bash
@@ -11,3 +11,9 @@ cp -v "${2}/javax/jms/javax.jms-api/2.0.1/javax.jms-api-2.0.1.jar" 'lib/'
 
 echo 'Copy slf4j-api version 2.0.17 ...'
 cp -v "${2}/org/slf4j/slf4j-api/2.0.17/slf4j-api-2.0.17.jar" 'lib/'
+
+echo 'Copy logback classic version 1.5.18 ...'
+cp -v "${2}/ch/qos/logback/logback-classic/1.5.18/logback-classic-1.5.18.jar" 'lib/'
+
+echo 'Copy logback core version 1.5.18 ...'
+cp -v "${2}/ch/qos/logback/logback-core/1.5.18/logback-core-1.5.18.jar" 'lib/'
